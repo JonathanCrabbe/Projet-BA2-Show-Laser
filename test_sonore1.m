c3225d0ff312da68c40433afd10a4714f4500900
Targamas <jonathan.cr1302@gmail.com>
2015-11-01 22:44:06 +0100

    Update test_sonore1.m

--- test_sonore1.m
+++ test_sonore1.m
@@ -11,9 +11,14 @@ nsamples = sps*nsecs;  %Pas d'échantillonage
 time = linspace(0, nsecs, nsamples); %Création de l'axe temps
 
 freq = 440 ;
-signal1 = sin(time*2*pi*freq);  %Premier signal (modifiez à votre guise)
-signal2 = signal1               %Second signal (modifiez à votre guise)     
-vecteur = [ signal1' signal2'];  %Création du signal total
+
+signal1 = sin(time*2*pi*freq);  %Signal galvo1
+
+signal2 = sin(time*2*pi*freq)./(time.*time);  %Signal galvo2
+
+signal3 = sin(time*2*pi*freq) .* (time ./40);  %Signal laser
+
+vecteur = [signal1'  signal2' signal3'];  %Création de la matrice contenant les signaux
 
 
 wavwrite(vecteur, sps, bps, 'audio.wav');   %Ecriture du fichier son
